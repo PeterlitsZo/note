c24c2095f8138e80b3f316ce3058b449629f0f89
Peterlits Zo <PeterlitsZo@Outlook.com>
2020-01-11 23:44:51 +0800

    upload command: new, init, list

--- run.py
+++ run.py
@@ -1,8 +1,3 @@
-#! /usr/env/python3
-
-from ruamel.yaml import YAML
-from collections import UserDict
-
 # ==============================================
 # about time: ----------------------------------
 import datetime
@@ -21,23 +16,13 @@ def add_indent(string, indent_int):
     indent = indent_int * ' '
     return indent + f"\n{indent}".join(string.split('\n'))
 
-def list_dict_obj(dict_data:'dict or non-dict'):
-    if isinstance(dict_data, (dict, UserDict)):
-        flag = 'begin'
-        for key, value in dict_data.items():
-            result = result + '\n' if flag == 'non-begin' else ''
-            flag = 'non-begin'
-            if not isinstance(value, dict):
-                result += f'{key}: {value}'
-            else:
-                result += f'{key}:\n' + add_indent(list_dict_obj(value), 4)
-    else:
-        result = str(dict_data)
-    return result
-
 
 # ===============================================
 # about yaml: -----------------------------------
+from ruamel.yaml import YAML
+from collections import UserDict
+
+
 class data(UserDict):
     _yamler = YAML()
     def __init__(self, path):
@@ -54,61 +39,115 @@ class data(UserDict):
             super_ = super()
             self_ = self
             def result_func(*args, **kwargs):
-                result = super_.__getattribute__(name)
                 result = getattr(super_, name)(*args, **kwargs)
-                self_._write()
+                self_.write()
                 return result
             return result_func
         else:
             return super().__getattribute__(name)
 
-    def _write(self):
+    def __delitem__(self, key):
+        super().__delitem__(key)
+        self.write()
+    
+    def __setitem__(self, index, value):
+        super().__setitem__(index, value)
+        self.write()
+
+    def del_file(self):
+        self.path.unlink()
+        del self
+
+    def _touch_data_file(self):
         if not self.path.exists():
             for parent_path in reversed(self.path.parents):
                 if not parent_path.exists():
                     parent_path.mkdir()
             else:
                 self.path.touch()
+        
+    def write(self):
+        self._touch_data_file()
         data._yamler.dump(self.data, self.path)
-
-    def __delitem__(self, key):
-        super().__delitem__(key)
-        self._write()
-    
-    def __setitem__(self, index, value):
-        super().__setitem__(index, value)
-        self._write()
 
 
 # ===============================================
 # about noteobject: -----------------------------
-class hasher(object):
-    def __init__(self, hash, cwd:'Path'):
-        self.cwd = cwd
-        self.hash = hash
-        self.path = self._get_path()
+class nodeobject_env(object):
+    def __init__(self, cwd, current_node=None):
+        self.cwd = cwd / 'pigtime' / 'object'
+        if current_node:
+            self.current = self.get_nodeobject(current_node)
+        else:
+            self.current = None
+        self._reload_hash_list()
 
-    def _get_path(self):
-        hash_str = self.hash
-        path = self.cwd / 'pigtime' /  'object' / hash_str[:2] / (hash_str[2:] + '.yml')
+    def __str__(self):
+        return str(self.current)
+
+    def _reload_hash_list(self):
+        self.hash_list = []
+        if self.cwd.exists():
+            for hash_dir in self.cwd.iterdir():
+                for hash_file in hash_dir.iterdir():
+                    self.hash_list.append(hash_dir.name + hash_file.name.rsplit('.', 1)[0])
+
+    def _get_full_hash(self, hash):
+        result = []
+
+        self._reload_hash_list()
+        for item in self.hash_list:
+            if item.startswith(hash):
+                result.append(item)
+        if len(result) > 1:
+            print('the given hash is too short to get a full one')
+            print('... or this is a wrong hash')
+        elif len(result) == 0:
+            print(f'there is no hash match {hash}')
+        else:
+            return result[0]
+
+    def _sha1_string(self, string):
+        import hashlib
+        hashed_string = string.encode('utf-8')
+        return hashlib.sha1(hashed_string).hexdigest()
+
+    def get_path(self, hash, existed=True):
+        full_hash = hash
+        if existed:
+            full_hash = self._get_full_hash(hash)
+        path = self.cwd / full_hash[:2] / (full_hash[2:] + '.yml')
         return path
+
+    def get_nodeobject(self, hash):
+        return nodeobject(hash, self)
+    
+    def new(self, type):
+        init_time = long_time()
+        hash = self._sha1_string(init_time)
+        
+        new_node_data = data(self.get_path(hash, existed=False))
+        new_node_data['type'] = type
+        new_node_data['init_time'] = init_time
+        return nodeobject(hash, self)
 
 
 class nodeobject(data):
-    def __new__(cls, hash, cwd:'Path'):
-        node_data = data(hasher(hash, cwd).path)
-        if node_data['type'] == 'note':
+    def __new__(cls, hash, env:'nodeobject_env'):
+        node_type = data(env.get_path(hash))['type']
+        if node_type == 'note':
             cls = nodeobject_note
-        elif node_data['type'] == 'tree':
+        elif node_type == 'tree':
             cls = nodeobject_tree
         else:
             raise ValueError('Unexcepection value {}'.format(node_data['type']))
-        return cls._init(hash, cwd)
+        return cls._init(hash, env)
 
     @classmethod
-    def _init(cls, hash, cwd):
+    def _init(cls, hash, env):
         self = object.__new__(cls)
-        super(nodeobject, self).__init__(hasher(hash, cwd).path)
+        super(nodeobject, self).__init__(env.get_path(hash))
+        self.env = env
         self.hash = hash
         self.type = self['type']
         return self
@@ -117,26 +156,34 @@ class nodeobject(data):
         # do nothing
         pass
 
-    @classmethod
-    def new(cls, cwd, type):
-        import hashlib
-        init_time = long_time()
-        hashed_string = (init_time.encode('utf-8'))
-        sha1 = hashlib.sha1(hashed_string)
-        hash_num = sha1.hexdigest()
-        
-        new_tree = data(hasher(hash_num, cwd).path)
-        new_tree['type'] = type
-        new_tree['init_time'] = init_time
-        return nodeobject(hash_num, cwd)
+    def rm(self):
+        for parent in self['parent']:
+            nodeobject(parent, self.env)['value'].pop(self.hash)
+        self.del_file()
+        if len(list(self.path.parent.iterdir())) == 0:
+            self.path.parent.unlink()
 
 
 class nodeobject_tree(nodeobject):
-    pass
+    def list(self):
+        print(str(self))
+
+    def __str__(self):
+        result_head = self.hash[:6] + f"...:(type: {self['type']})\n"
+        result = []
+        for nodeobject_hash in self['value']:
+            node = nodeobject(nodeobject_hash, self.env)
+            result.append(str(node))
+        result = result_head + add_indent('\n'.join(result), 4)
+        return result
 
 
 class nodeobject_note(nodeobject):
-    pass
+    def __str__(self):
+        result = '* {}...:\n'.format(self.hash[:6])
+        result_connect = '\n'.join([f'{c}: {self[c]}' for c in self if c != 'text'])
+        result += add_indent(result_connect, 4)
+        return result
 
 
 # ===============================================
@@ -144,9 +191,9 @@ class nodeobject_note(nodeobject):
 class commander_base(object):
     def __init__(self, cwd):
         self.cwd = cwd
-        self.toc = data(cwd /'toc.yml')
         self.config = data(cwd / 'config.yml')
-        self.current = nodeobject(self.config['current'], self.cwd)
+        current = self.config['current'] if 'current' in self.config else None
+        self.obj_env = nodeobject_env(self.cwd, current)
         self.file_name = self.config['file_name']
         self.file = lambda suffix: cwd / (self.file_name + '.' + suffix)
 
@@ -154,7 +201,7 @@ class commander_base(object):
         import subprocess
         subprocess.run(command, cwd=self.cwd, stdout=stdout)
 
-    def rm(self, *file_paths):
+    def rm(self, file_paths:'iterable'):
         for file_path in file_paths:
             print('[rm]:', file_path.name)
             (self.cwd / file_path).unlink()
@@ -163,9 +210,7 @@ class commander_base(object):
         '''suffix_without_dot: look like 'pdf|tex|yml|' or else'''
         files = self.cwd.iterdir()
         suffixs = suffix_without_dot.split('|')
-        for file in files:
-            if file.suffix[1:] in suffixs:
-                self.rm(file)
+        self.rm(file for file in files if file.suffix[1:] in suffixs)
 
     def touch(self, string, path):
         with open(self.cwd / path, 'wt', encoding='utf-8') as file:
@@ -173,8 +218,11 @@ class commander_base(object):
             file.write(string)
 
     def list_toc(self):
-        print(list_dict_obj(self.toc))
-
+        print(self.current)
+    
+    def list(self):
+        print(self.obj_env)
+    
     def rm_useless(self):
         self.rm_by_suffix('aux|log|cls|tex|txt')
 
@@ -191,7 +239,6 @@ class commander(commander_base):
         super().__init__(cwd)
 
     def reload(self):
-        self.toc = data(self.cwd /'toc.yml')
         self.config = data(self.cwd / 'config.yml')
 
     def xelatex_it(self):
@@ -200,40 +247,27 @@ class commander(commander_base):
         self.run_ex(['xelatex', self.file('tex')], open(self.file('output.txt'), 'w+'))
         print('end of latex')
 
-    # def new(self):
-    #     input_ = input("enter the file's name > ")
-    #     index = '{:0>3}'.format(max(int(key) for key in self.toc) + 1)
-    #     path = self.cwd / 'parts' / '{}.tex'.format(index)
-        
-    #     self.toc[index] = {'name': input_}
-
-    #     import datetime
-    #     today = datetime.date.today()
-    #     # touch with: '\section{input_}\n\timetx{today_}
-    #     self.touch(f'\\section{{{input_}}}\n\\timetx{{{today}}}\n\n', path)
-        
-    #     self.run_ex(['vim', path])
-    # <<<<< older
-    # ===========================================
-    # >>>>> newer
     def init(self):
         if 'current' in self.config:
             print('it is already inited')
             return
-        root = nodeobject.new(self.cwd, 'tree')
+        root = self.obj_env.new('tree')
         root['value'] = []
         self.config['current'] = root.hash
+        self.obj_env.current = root
 
     def new(self):
-        note = nodeobject.new(self.cwd, 'note')
+        note = self.obj_env.new('note')
 
         title = input("enter the note's title > ")
         note['title'] = title
-        self.current['value'].append(note.hash)
+        note['parent'] = self.obj_env.current.hash
+        self.obj_env.current['value'].append(note.hash)
+        self.obj_env.current.write()
         
         self.run_ex(['vim', self.file('temp.tex')])
-        if self.file('.temp.tex').exists():
-            note.path.write_text(self.file('temp.tex').read_text(encoding='utf-8'), encoding='utf-8')
+        if self.file('temp.tex').exists():
+            note['text'] = self.file('temp.tex').read_text(encoding='utf-8')
             self.file('temp.tex').unlink()
         else:
             # !
@@ -241,10 +275,9 @@ class commander(commander_base):
 
     def rm_note(self):
         self.list_toc()
-        input_ = input('enter enter the index > ')
-
-        self.toc.pop(input_)
-        (self.cwd / 'parts' / f'{input_}.tex').unlink()
+        input_ = input("enter the node's hash > ")
+        node = nodeobject(input_, self.cwd)
+        node.rm()
 
     def edit_file(self):
         self.list_toc()
@@ -268,7 +301,7 @@ class commander(commander_base):
         try:
             self.run_ex(['chrome', self.file('pdf')])
         except:
-            print('need to add chrome in PATH, or change the run.py file\'s 138 line')
+            print('need to add chrome in PATH, or change the run.py file\'s 291 line')
 
 
 # ===============================================
@@ -302,6 +335,7 @@ if __name__ == '__main__':
         input_ = input('> ')
         com.reload()
         try:
+            # !this
             if input_ == 'run':
                 com.run_it(Test=Test)
             elif input_ == 'help':
@@ -311,11 +345,13 @@ if __name__ == '__main__':
             elif input_ == '?q':
                 break
             elif input_ == 'list':
-                com.list_toc()
+                com.list()
+            # !this
             elif input_ == 'rm':
                 com.rm_note()
             elif input_ == 'look':
                 com.look_it()
+            # !this
             elif input_ == 'edit':
                 com.edit_file()
             elif input_ == 'init':
@@ -324,10 +360,9 @@ if __name__ == '__main__':
                 continue
             else:
                 print(f'no command named {input_}')
-            print()
         except Exception as e:
             import traceback
             print(e, '\n')
             print('-' * 50)
             traceback.print_exc()
-            print()
+        print()
